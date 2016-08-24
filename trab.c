416928477023e7e7a824d1bff54122dfdc32d73d
memphis47 <rafarocar47@gmail.com>
2016-08-24 13:59:25 +0000

    INT: Feito validação de porta e ip

--- trab.c
+++ trab.c
@@ -5,6 +5,7 @@
 #define MAX_STRING_ARRAY 1
 #define IP_FIELD_SIZE 16
 #define IP_REGEX "^([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\\.(([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))|(([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\\-([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))))$"
+#define PORT_REGEX "(^(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-5][0-9][0-9][0-9][0-9]|6[0-4][0-9][0-9][0-9]|65[0-4][0-9][0-9]|655[0-2][0-9]|6553[0-6])|(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-5][0-9][0-9][0-9][0-9]|6[0-4][0-9][0-9][0-9]|65[0-4][0-9][0-9]|655[0-2][0-9]|6553[0-6])\\-([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-5][0-9][0-9][0-9][0-9]|6[0-4][0-9][0-9][0-9]|65[0-4][0-9][0-9]|655[0-2][0-9]|6553[0-6])))$)"
 
 int getRange(char *str, char *splitter){
     char *token = strtok(str, splitter);
@@ -73,23 +74,21 @@ int generateIPs(char ***ips, int ipField, int range, char* ip){
     return 1;
 }
 
-void validateIP(char *ip)
-{
-	/* aloca espaço para a estrutura do tipo regex_t */
-	regex_t reg;
+int regexValidation(char *strValidate, char * pattern){
 
-	/* compila a ER passada em argv[1]
+    regex_t reg;
+    /* compila a ER passada em argv[1]
 	 * em caso de erro, a função retorna diferente de zero */
-	if (regcomp(&reg , IP_REGEX, REG_EXTENDED|REG_NOSUB) != 0) {
+	if (regcomp(&reg , pattern, REG_EXTENDED|REG_NOSUB) != 0) {
 		fprintf(stderr,"erro regcomp\n");
 		exit(1);
 	}
 	/* tenta casar a ER compilada (&reg) com a entrada (argv[2]) 
 	 * se a função regexec retornar 0 casou, caso contrário não */
-	if ((regexec(&reg, ip, 0, (regmatch_t *)NULL, 0)) == 0)
-		printf("Casou\n");
+	if ((regexec(&reg, strValidate, 0, (regmatch_t *)NULL, 0)) == 0)
+		return 1;
 	else
-		printf("Não Casou\n");
+		return 0;
 }
 
 int main(int argc, char **argv){
@@ -100,35 +99,46 @@ int main(int argc, char **argv){
     int portaRange;
   
     if(argv[1] != NULL){
-        validateIP(argv[1]);
-        ip = malloc(strlen(argv[1]));
-        strcpy(ip, argv[1]);
-        ipRange = getRange(ip, "-");
-        ipLastField = getLastField(ip);
-        if(ipRange < ipLastField){
-            printf("\nErro:\nValor do range do ip menor do que valor do campo ip\n");
+        if(regexValidation(argv[1], IP_REGEX)){
+            ip = malloc(strlen(argv[1]));
+            strcpy(ip, argv[1]);
+            ipRange = getRange(ip, "-");
+            ipLastField = getLastField(ip);
+            if(ipRange < ipLastField){
+                printf("\nErro:\nValor do range do ip menor do que valor do campo ip\n");
+                return 0;
+            }
+            ip = ipSplit(ip);
+            printf("\nIP: %s\n", ip);
+            printf("Last field: %d\n", ipLastField);
+            printf("Range: %d\n\n", ipRange);
+        }
+        else{
+            printf("\nFormato do IP inválido\n");
             return 0;
         }
-        ip = ipSplit(ip);
-        printf("\nIP: %s\n", ip);
-        printf("Last field: %d\n", ipLastField);
-        printf("Range: %d\n\n", ipRange);
     }
     else{
         printf("\nErro:\nValor do ip não pode ser nulo\n");
         return 0; 
     }
     if(argv[2] != NULL){
-        char *strPorta = malloc(strlen(argv[2]));
-        strcpy(strPorta, argv[2]);
-        portaRange = getRange(strPorta, "-");
-        porta = atoi(strPorta);
-        if(portaRange < porta){
-            printf("\nErro:\nValor do range da porta menor do que valor do campo porta\n");
+        if(regexValidation(argv[2], PORT_REGEX)){
+            char *strPorta = malloc(strlen(argv[2]));
+            strcpy(strPorta, argv[2]);
+            portaRange = getRange(strPorta, "-");
+            porta = atoi(strPorta);
+            if(portaRange < porta){
+                printf("\nErro:\nValor do range da porta menor do que valor do campo porta\n");
+                return 0;
+            }
+            printf("Porta: %d\n", porta);
+            printf("Range: %d\n\n", portaRange); 
+        }
+        else{
+            printf("\nFormato da porta inválido\n");
             return 0;
         }
-        printf("Porta: %d\n", porta);
-        printf("Range: %d\n\n", portaRange); 
     }
    
     char ***ips = calloc(1, sizeof(char **));
